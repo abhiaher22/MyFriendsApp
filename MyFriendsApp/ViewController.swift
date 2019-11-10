b6566ed479477902e98bbc309b818e96b253bac0
abhiaher22 <abhiaher22@gmail.com>
2019-11-10 10:52:27 -0800

    Initial Commit

---
+++ MyFriendsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFriendsApp
+//
+//  Created by Abhijeet Aher on 11/10/19.
+//  Copyright © 2019 Abhijeet Aher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyFriendsAppTests/MyFriendsAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyFriendsAppTests.swift
+//  MyFriendsAppTests
+//
+//  Created by Abhijeet Aher on 11/10/19.
+//  Copyright © 2019 Abhijeet Aher. All rights reserved.
+//
+
+import XCTest
+@testable import MyFriendsApp
+
+class MyFriendsAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
