60b95a88df446bc14178f3d75ff6e271564908c6
abhiaher22 <abhiaher22@gmail.com>
2019-11-10 12:23:15 -0800

    Fetch data and display on table view

---
+++ MyFriendsApp/Connection.swift
@@ -0,0 +1,15 @@
+//
+//  Connection.swift
+//  MyFriendsApp
+//
+//  Created by Abhijeet Aher on 11/10/19.
+//  Copyright © 2019 Abhijeet Aher. All rights reserved.
+//
+
+import Foundation
+
+struct Connection: Codable{
+    var id: UUID
+    var name: String
+    
+}
